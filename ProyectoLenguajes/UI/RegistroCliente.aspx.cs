209f55560d5c421f7b47125d738e757b1fa9a522
Miguel Egoavil <54037469+TheMikox16@users.noreply.github.com>
2021-07-02 12:23:25 -0600

    .
    .

---
+++ ProyectoLenguajes/UI/RegistroCliente.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CapaLogicaAdministracion;
+
+
+namespace ModuloAdministracion
+{
+    public partial class RegistroCliente : System.Web.UI.Page
+    {
+
+        private LogicaAdministracion logica;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            logica = new LogicaAdministracion();
+            Session["logica"] = logica;
+            modificar_btn.Enabled = false;
+            email_txt.Disabled = false;
+        }
+    }
+}
